0a72b6c281c865971051cb3638cf47782d13a567
deen1113 <khan.de@northeastern.edu>
2024-10-28 16:19:20 -0400

    added direction enum

---
+++ src/model/Direction.java
@@ -0,0 +1,14 @@
+package model;
+
+/**
+ * This enum represents the 4 directions that are represented
+ * on each card. Each direction interacts with the adjacent one
+ * on the board.
+ */
+public enum Direction {
+
+  NORTH(0), EAST(1), SOUTH(2), WEST(3);
+
+  Direction(int i) {
+  }
+}
